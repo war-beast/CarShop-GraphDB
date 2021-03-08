8ed3d561981a8292e7e30ab5dfa0a1bb3f91df3d
Vladimir Sharapov <v.sharapov@artlogics.ru>
2021-03-08 17:18:40 +0300

    Реализовал выборку блоков

--- CarShop.BLL/Services/StorageService.cs
+++ CarShop.BLL/Services/StorageService.cs
@@ -1,5 +1,6 @@
 ﻿using CarShop.BLL.Interfaces;
 using CarShop.BLL.Models;
+using Microsoft.Extensions.Options;
 using Neo4jClient;
 using System;
 using System.Collections.Generic;
@@ -13,34 +14,54 @@ namespace CarShop.BLL.Services
 		#region private memebers
 
 		private readonly IGraphClient _client;
+		private readonly GraphConnection _graphConnection;
 
 		#endregion
 
 		#region constructor
 
-		public StorageService(IGraphClient client)
+		public StorageService(IGraphClient client, IOptionsSnapshot<GraphConnection> options)
 		{
 			_client = client;
+			_graphConnection = options?.Value ?? throw new ArgumentNullException(nameof(options.Value));
 		}
 
 		#endregion
 
 		public async Task<IReadOnlyList<Block>> GetBlocks()
 		{
-			var result = await _client
+			var result = (await _client
 				.Cypher
-				.Match("(b:Block)")
-				.Return(b => b.CollectAs<Block>())
-				.ResultsAsync;
+				.Match("(p:SparePart)-[rel:PART_OF]->(b:Block)")
+				.Return((p, b) => new {
+					Blocks = b.CollectAs<Block>(),
+					Parts = p.CollectAs<SparePart>()
+				})
+				.ResultsAsync)
+				.ToList();
 
-			return result
-				.First()
-				.Select(x => new Block
+			//TODO: Можно получить данные через IDriver из библиотеки Neo4j.Driver, но маппить придётся в ручную
+			//var driver = GraphDatabase.Driver($"{_graphConnection.Host}:{_graphConnection.Port}", AuthTokens.Basic(_graphConnection.User, _graphConnection.Password));
+			//var session = driver.AsyncSession(o => o.WithDatabase(_client.DefaultDatabase));
+			//var cursor = await session.RunAsync("MATCH (p:SparePart)-[rel:PART_OF]->(b:Block) RETURN p, b, rel");
+
+			var blocks = result.First().Blocks;
+			var spareParts = result.First().Parts;
+
+			var resultList = spareParts.Select((x, idx) => 
 				{
-					Name = x.Name,
-					Parts = new List<SparePart>()
+					x.Block = blocks.ElementAt(idx);
+					return x;
+				})
+				.GroupBy(x => x.Block.Name)
+				.Select(x => new Block()
+				{
+					Name = x.Key,
+					Parts = x.ToList()
 				})
 				.ToList();
+
+			return resultList;
 		}
 
 		public async Task<IReadOnlyList<SparePart>> GetParts()
