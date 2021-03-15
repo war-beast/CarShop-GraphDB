e8373f1f5bf614ab00957d9010b5987599c4d54b
Vladimir Sharapov <v.sharapov@artlogics.ru>
2021-03-15 17:07:17 +0300

    Добавил пакет NEST

--- CarShop/Startup.cs
+++ CarShop/Startup.cs
@@ -1,20 +1,15 @@
+using CarShop.BLL.Models;
+using CarShop.Data;
+using CarShop.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Neo4jClient;
-using CarShop.BLL.Models;
-using CarShop.Data;
-using CarShop.Extensions;
+using System;
 
 namespace CarShop
 {
